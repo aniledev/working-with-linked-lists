a0295dd996c7a2f4a965573edd136ad94bf723af
aniledev <elinamcgill@gmail.com>
2021-02-17 01:32:37 -0500

    FEAT: write method to find a value in a linked list

--- linked-list.js
+++ linked-list.js
@@ -25,30 +25,28 @@ class LinkedList {
       tempNode.next = new _Node(item, null);
     }
   }
-  
 
   // item can be inserted in the middle
-    
-  find(item) { 
+
+  find(item) {
     // Start at the head
     let currNode = this.head;
-    // If the list is empty
+    // If the list is empty return null
     if (!this.head) {
-        return null;
+      return null;
     }
-    // Check for the item 
+    // Check for the item as long as the item is not found
     while (currNode.value !== item) {
-        /* Return null if it's the end of the list 
+      /* Return null if it's the end of the list 
            and the item is not on the list */
-        if (currNode.next === null) {
-            return null;
-        }
-        else {
-            // Otherwise, keep looking 
-            currNode = currNode.next;
-        }
+      if (currNode.next === null) {
+        return null;
+      } else {
+        // Otherwise, keep looking
+        currNode = currNode.next;
+      }
     }
-    // Found it
+    // Found it when currNode.value === item
     return currNode;
-}
+  }
 }
