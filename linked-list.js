56bebe73e6f89f07ec1371c8d313b3aec38248ef
aniledev <elinamcgill@gmail.com>
2021-02-17 01:19:11 -0500

    FEAT: write method to insert a new node at the beginning of linked list

--- linked-list.js
+++ linked-list.js
@@ -1,10 +1,15 @@
 class LinkedList {
-    constructor() {
-        // head indicates the beginning of the list which happens to be empty
-        this.head = null;
-    }
+  constructor() {
+    // head indicates the beginning of the list which happens to be empty
+    this.head = null;
+  }
 
-    // item can be inserted at the beginning
-    // item can be inserted at the end
-    // item can be inserted in the middle
+  // item can be inserted at the beginning
+  insertFirst(item) {
+    // create a new node item and point the head to that new code
+    this.head = new _Node(item, this.head);
+  }
+    
+  // item can be inserted at the end
+  // item can be inserted in the middle
 }
