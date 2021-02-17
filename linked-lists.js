eb05b336b07b3ce5f7064708bcce80a0eb679cea
aniledev <elinamcgill@gmail.com>
2021-02-17 01:13:31 -0500

    FEAT: instantiate Node class

---
+++ linked-lists.js
@@ -0,0 +1,7 @@
+class _Node {
+  // each item in a linked list is a node that stores a value and stores address of next node
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
