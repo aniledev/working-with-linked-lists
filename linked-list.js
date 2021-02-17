c85ab253b0330aec1e1b88be5f56b44e2bc4375b
aniledev <elinamcgill@gmail.com>
2021-02-17 01:17:54 -0500

    CHORE: write insertion pseudocode

--- linked-list.js
+++ linked-list.js
@@ -3,3 +3,8 @@ class LinkedList {
         // head indicates the beginning of the list which happens to be empty
         this.head = null;
     }
+
+    // item can be inserted at the beginning
+    // item can be inserted at the end
+    // item can be inserted in the middle
+}
